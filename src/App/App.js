f749f095fe1002fd05f4846332b63ebf45f21e1f
Timothy Harley <timothy.d.harley@gmail.com>
2018-12-13 00:38:45 -0600

    added profile component that only loads when logged in

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import connection from '../Helpers/Data/connection';
 import Auth from '../Components/Auth/Auth';
+import Profile from '../Components/Profile/profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <Auth />
+        <Profile />
       </div>
     );
   }
